ef5339e8f95175926313bbeda2d36fed0162e32f
Anh Quan <anhquanhdtl@gmail.com>
2022-12-29 06:53:54 +0700

    data access using select

--- src/main/java/com/example/springbootjdbcapi/SpringbootJdbcApiApplication.java
+++ src/main/java/com/example/springbootjdbcapi/SpringbootJdbcApiApplication.java
@@ -1,14 +1,29 @@
 package com.example.springbootjdbcapi;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootJdbcApiApplication {
+public class SpringbootJdbcApiApplication implements CommandLineRunner {
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
 
-	public static void main(String[] args) {
-		
-		SpringApplication.run(SpringbootJdbcApiApplication.class, args);
+	@Autowired
+	PlayerDAO playerDao;
+	@Override
+	public void run(String... args) throws Exception {
+		logger.info("All Players Data: {}"+ playerDao.getAllPlayers());
+
+		logger.info("Player with Id 3: {}"+ playerDao.getPlayerById(3));
 	}
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringbootJdbcApiApplication.class, args);
+
+	}
+
 
 }
